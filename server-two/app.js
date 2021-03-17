17f26283f6829e7ccf5a4d2773b6f5ccb1cea0ba
Matias Jalava <matias@elm.fi>
2021-03-17 15:27:18 +0200

    pug template working

---
+++ server-two/app.js
@@ -0,0 +1,25 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+      name: 'Frank',
+      age: 6,
+      weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+  console.log(`Server-two listening at http://localhost:${port}`);
+});
